ca293d05ac5d4f9cecd7aa75fd1a1134d693ae0b
Daniel Underwood <daniel.underwood13@gmail.com>
2018-11-12 19:56:56 -0500

    Remove unneeded AddNLog call in startup

--- LidarrAPI/Startup.cs
+++ LidarrAPI/Startup.cs
@@ -80,8 +80,6 @@ namespace LidarrAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
         {
-            loggerFactory.AddNLog();
-            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
